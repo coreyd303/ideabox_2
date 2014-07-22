1f531ecbd99cabdca75ecb338d599dd28ecd543c
Corey Davis <coreyd303@gmail.com>
2014-07-22 10:40:12 -0600

    I3: App can add and delete ideas

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require './idea'
 
 class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
   not_found do
     erb :error
   end
@@ -10,15 +12,17 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   get '/' do
-   erb :index
+   erb :index, locals: {ideas: Idea.all}
   end
 
   post '/' do
-    # 1. Create an idea based on the form parameters
-    idea = Idea.new
-    # 2. Store it
+    idea = Idea.new(params['idea_title'], params['idea_description'])
     idea.save
-    # 3. Send us back to the index page to see all ideas
-    "Creating an IDEA!"
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+    redirect '/'
   end
 end
