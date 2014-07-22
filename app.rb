ba9f8f7b7f0bda75130830bdbd7e6eaf388d87de
Corey Davis <coreyd303@gmail.com>
2014-07-22 09:35:14 -0600

    I1: App can create new ideas

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
+require './idea'
+
 class IdeaBoxApp < Sinatra::Base
+  not_found do
+    erb :error
+  end
+
   configure :development do
     register Sinatra::Reloader
   end
+
   get '/' do
    erb :index
+  end
+
+  post '/' do
+    # 1. Create an idea based on the form parameters
+    idea = Idea.new
+    # 2. Store it
+    idea.save
+    # 3. Send us back to the index page to see all ideas
+    "Creating an IDEA!"
   end
 end
